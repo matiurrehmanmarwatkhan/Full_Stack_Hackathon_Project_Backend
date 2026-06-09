68b40ca63920872eb245cf678bad700bc89a5052
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-06-09 10:15:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 2000;
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://full-stack-hackathon-project-fronte.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
