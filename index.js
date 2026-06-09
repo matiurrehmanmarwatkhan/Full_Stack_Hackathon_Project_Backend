56f09d2dc213fda8d835276e0e084e9e20318bd4
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-06-09 09:59:42 +0500

    Remove URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 2000;
 app.use(
   cors({
-    origin: ["https://mern-stack-hackathon-frontend.vercel.app", "http://localhost:5173"],
+    origin: ["http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
